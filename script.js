74a0d06fc1d5ba6198c86018e1ec4ed5c001c2a9
AmonFlame <matt.mcb@hotmail.co.uk>
2026-02-11 17:01:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function openInstagram(e) {
+  e.preventDefault();
 
+  const app = "instagram://user?username=kaigosasuke";
+  const web = "https://www.instagram.com/kaigosasuke";
+
+  window.location = app;
+
+  setTimeout(() => {
+    window.location = web;
+  }, 500);
+}
